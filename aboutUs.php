40045b64b494e8c97be47811f862c1f4e4f95ea5
Ankit Singh <singhankit1833@gmail.com>
2023-10-28 21:17:56 +0530

    Add files via upload

---
+++ aboutUs.php
@@ -0,0 +1,95 @@
+<html>
+<head>
+	<title>Nazareth</title>
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+<link rel="stylesheet" type="text/css" href="aboutus.css">
+<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"> -->
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+<style >
+     .form-control {
+    border-radius: 0.75rem;
+}
+</style>
+
+<body>
+    <div class="nav-bar">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav" >
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#" style="margin-top: 10px;margin-left:-65px;font-family: 'IBM Plex Sans', sans-serif; color:blue;"><h4><img src="images\nazareth.png" alt="Girl in a jacket" width="100" height="100"></img><b>&nbsp NAZARETH HOSPITALS</b></h4></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item" style="margin-right: 40px;">
+            <a class="nav-link js-scroll-trigger" href="index.php" style="font-family: 'IBM Plex Sans', sans-serif;"><h4>HOME</h4></a>
+          </li>
+
+          <li class="nav-item" style="margin-right: 40px;">
+            <a class="nav-link js-scroll-trigger" href="login.php" style="font-family: 'IBM Plex Sans', sans-serif;"><h4>LOGIN</h4></a>
+          </li>
+  
+          <li class="nav-item" style="margin-right: 40px;">
+            <a class="nav-link js-scroll-trigger" href="aboutUs.html" style="font-family: 'IBM Plex Sans', sans-serif;"><h4>ABOUT US</h4></a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="contactUs.html" style="font-family: 'IBM Plex Sans', sans-serif;"><h4>CONTACT US</h4></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  </div>
+  <!-- <div class="text-content">
+  <p><strong>Nazareth Hospital</strong> situated in the heart of the city of Shillong,. Nazareth Hospital is a voluntary, Non-profit Organization started in 1959 as an out-patient treatment center. The Archdiocese of Shillong, the legal owner of Nazareth Hospital has entrusted its administration and management to the Sisters of the Holy Cross Menzingen( North Indian Province) in the year 1988.</p>
+<p>The history of Nazareth Hospital is a history of progressive expansion and indeed, it is still travelling along its path into the future. The Hospital with its rich heritage of service was founded in response to the needs of society. In the true spirit of service it continued to evolve in response to societal needs. Since its inception, it has gone through many stages of development and progress, providing emergency services, general services and specialty services round the clock. It holds the status of a referral and teaching institute. It is in the front line, both in academics and patient services.</p> 
+</div> -->
+
+<div class="card-deck ">
+  <div class="card">
+    <img class="card-img-top" src="images/telecom.png" alt="Card image cap" width="20px">
+    <div class="card-body">
+      <h5 class="card-title">Telemedicine</h5>
+      <p class="card-text">Telemedicine is the use of technology to provide healthcare services remotely, allowing patients to consult with healthcare professionals via video calls or phone.</p>
+    </div>
+    
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="images\appoint.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Appointment Scheduling</h5>
+      <p class="card-text">It involves the process of booking and managing medical appointments for patients, typically done through online platforms ensuring efficient and organized patient care.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="images/record.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Patient Record Management</h5>
+      <p class="card-text">This refers to the digital storage and organization of patient medical records, including health history, diagnoses, treatments, and test results, making it easier for healthcare providers to access and update patient information.</p>
+    </div>
+
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="images/health.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Health Information Sharing</h5>
+      <p class="card-text">It involves securely exchanging patient health data between healthcare providers, enabling coordinated care and better-informed medical decisions while maintaining patient privacy.</p>
+    </div>
+
+  </div>
+</div>
+      </body>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    </html>
